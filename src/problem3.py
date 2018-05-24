3032046d12ced328eab17e4073dea832bf98d3cf
nebulaenb <max3@rose-hulman.edu>
2018-05-24 13:45:59 -0400

    change

--- src/problem3.py
+++ src/problem3.py
@@ -2,9 +2,9 @@
 Final exam, problem 3.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Xiaoyu Ma.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 import time
@@ -93,16 +93,45 @@ def problem3(point, circle1, circle2, window):
       :type window:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
+    point.attach_to(window)
+    circle1.attach_to(window)
+    circle2.attach_to(window)
 
+    line1 = rg.Line(point, circle1.center)
+    line2 = rg.Line(point, circle2.center)
+    line3 = rg.Line(circle1.center, circle2.center)
+
+    line1.color = circle1.fill_color
+    line2.color = circle1.fill_color
+    line3.color = circle1.fill_color
+
+    line1.attach_to(window)
+    line2.attach_to(window)
+    line3.attach_to(window)
+
+    line4 = rg.Line(line1.get_midpoint(), line2.get_midpoint())
+    line5 = rg.Line(line1.get_midpoint(), line3.get_midpoint())
+    line6 = rg.Line(line2.get_midpoint(), line3.get_midpoint())
+
+    line4.color = circle2.fill_color
+    line5.color = circle2.fill_color
+    line6.color = circle2.fill_color
+
+    line4.attach_to(window)
+    line5.attach_to(window)
+    line6.attach_to(window)
+
+    window.render()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # The   try .. except   prevents error messages on the console from being
 # intermingled with ordinary output to the console.
 # -----------------------------------------------------------------------------
+
 
 # To allow color-coding the output to the console:
 USE_COLORING = True  # Change to False to revert to OLD style coloring
